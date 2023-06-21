9fcc0fe4c3a128bf1fe7873643b7519aa9b9796c
Jack Windham <jckwind11@gmail.com>
2023-06-21 14:21:56 -0400

    .

--- Sources/NavigationTransitions/NavigationTransition+UIKit.swift
+++ Sources/NavigationTransitions/NavigationTransition+UIKit.swift
@@ -162,7 +162,7 @@ extension UINavigationController {
 		if edgePanRecognizer == nil {
 			edgePanRecognizer = UIScreenEdgePanGestureRecognizer()
 			if transition.isVertical {
-				edgePanRecognizer.edges = [.left, .top]
+				edgePanRecognizer.edges = .left
 			} else {
 				edgePanRecognizer.edges = .left
 			}
